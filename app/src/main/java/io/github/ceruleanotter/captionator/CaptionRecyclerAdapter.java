7aeea7ca1cbad70c1246e169f493caaf274c2a68
Lyla <lyla@udacity.com>
2017-01-01 15:47:56 -0800

    Refactor: Remove data binding, add butterknife

--- app/src/main/java/io/github/ceruleanotter/captionator/CaptionRecyclerAdapter.java
+++ app/src/main/java/io/github/ceruleanotter/captionator/CaptionRecyclerAdapter.java
@@ -1,30 +1,26 @@
 package io.github.ceruleanotter.captionator;
 
-import android.databinding.DataBindingUtil;
+
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.Query;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import io.github.ceruleanotter.captionator.databinding.CaptionRecyclerViewItemBinding;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.github.ceruleanotter.captionator.models.Caption;
-import io.github.ceruleanotter.captionator.models.CaptionatorImage;
 import timber.log.Timber;
 
 /**
  * Created by lyla on 12/26/16.
  */
 
-public class CaptionRecyclerAdapter extends FirebaseRecyclerAdapter<Caption,CaptionRecyclerAdapter.CaptionHolder> {
+public class CaptionRecyclerAdapter extends FirebaseRecyclerAdapter<Caption, CaptionRecyclerAdapter.CaptionHolder> {
 
     public static final String CAPTION_IMAGES_STORAGE_PATH = "captions_images";
 
@@ -33,20 +29,8 @@ public class CaptionRecyclerAdapter extends FirebaseRecyclerAdapter<Caption,Capt
     public CaptionRecyclerAdapter(Class<Caption> modelClass, int modelLayout, Class<CaptionHolder> viewHolderClass, Query ref) {
         super(modelClass, modelLayout, viewHolderClass, ref);
         mStorageLocation = FirebaseStorage.getInstance().getReference().child(CAPTION_IMAGES_STORAGE_PATH);
-
-
-
-
     }
 
-    @Override
-    public CaptionHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        CaptionRecyclerViewItemBinding binding = DataBindingUtil
-                .inflate(layoutInflater, viewType, parent, false);
-
-        return new CaptionHolder(binding);
-    }
 
     @Override
     protected void populateViewHolder(CaptionHolder viewHolder, Caption model, int position) {
@@ -55,29 +39,38 @@ public class CaptionRecyclerAdapter extends FirebaseRecyclerAdapter<Caption,Capt
 
 
     public static class CaptionHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        CaptionRecyclerViewItemBinding mBinding;
         Caption mCaption;
+        @BindView(R.id.author_text_view)
+        TextView authorTextView;
+        @BindView(R.id.caption_text_view)
+        TextView captionTextView;
+        @BindView(R.id.rating_text_view)
+        TextView ratingTextView;
+        @BindView(R.id.vote_up_image_button)
+        ImageButton voteUpImageButton;
+        @BindView(R.id.vote_down_image_button)
+        ImageButton voteDownImageButton;
 
 
-
-        public CaptionHolder(CaptionRecyclerViewItemBinding binding) {
-            super(binding.getRoot());
-            mBinding = binding;
-            mBinding.voteUpImageButton.setOnClickListener(this);
-            mBinding.voteDownImageButton.setOnClickListener(this);
+        public CaptionHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            voteUpImageButton.setOnClickListener(this);
+            voteDownImageButton.setOnClickListener(this);
         }
+
         public void bindCaption(Caption c) {
             mCaption = c;
-            mBinding.authorTextView.setText(c.getAuthor());
-            mBinding.captionTextView.setText(c.getCaption());
-            mBinding.ratingTextView.setText(Integer.toString(c.getUpvotes()-c.getDownvotes()));
+            authorTextView.setText(c.getAuthor());
+            captionTextView.setText(c.getCaption());
+            ratingTextView.setText(Integer.toString(c.getUpvotes() - c.getDownvotes()));
         }
 
         @Override
         public void onClick(View view) {
-            if (view == mBinding.voteDownImageButton) {
+            if (view == voteDownImageButton) {
                 Timber.d("Down vote");
-            } else if (view == mBinding.voteUpImageButton) {
+            } else if (view == voteUpImageButton) {
                 Timber.d("Up vote");
             }
         }
